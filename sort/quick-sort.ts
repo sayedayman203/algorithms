28ed5b420c368bff191a26e1dd1a399a645c71ec
sayedayman203 <sayedayman203@gmail.com>
2022-05-03 20:08:03 +0200

    quick-sort

--- sort/quick-sort.ts
+++ sort/quick-sort.ts
@@ -8,3 +8,5 @@ export const quick_sort = (arr: (number | string)[]): (string | number)[] => {
     return [...quick_sort(less), el, ...quick_sort(greater)];
   }
 };
+
+// console.log(quick_sort([11, 5, 39, 75, 16, 385, 1, 6, 77, 52]));
